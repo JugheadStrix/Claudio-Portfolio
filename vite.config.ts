05f93d663e1b710f099a85f7f2263b8f3e109186
Strix <kanto.claudio@gmail.com>
2025-03-18 19:14:33 +0300

    modification vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Claudio-Portfolio/",
   plugins: [react()],
   server: {
     host: true, // Permet d'écouter sur toutes les adresses réseau
